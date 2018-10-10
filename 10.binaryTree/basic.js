5b91544bc2227d60628e7bbb905b505b82869480
Carmen Liu <carmen.liujia@gmail.com>
2018-10-10 22:00:20 +1300

    binaryTree clear up

--- 10.binaryTree/basic.js
+++ 10.binaryTree/basic.js
@@ -1,48 +1,47 @@
-function Node(data, left, right) {
-  this.data = data;
-  this.left = left;
-  this.right = right;
-  this.show = show;
+function Node (data, left, right) {
+  this.data = data
+  this.left = left
+  this.right = right
+  this.show = show
 }
 
-function show() {
-  return this.data;
+function show () {
+  return this.data
 }
 
-
-function BST() {
-  this.root = null;
-  this.insert = insert;
+function BST () {
+  this.root = null
+  this.insert = insert
 }
 
-function insert(data) {
-  n = new Node(data, null,null,null);
+function insert (data) {
+  let n = new Node(data, null, null, null)
   if (this.root == null) {
     this.root = n
   } else {
     var current = this.root
     var parent
-    while(true) {
-      parent = current;
-      if ( data < current.data) {
+    while (true) {
+      parent = current
+      if (data < current.data) {
         current = current.left
-        if(current == null) {
-          parent.left = n;
-          break;
+        if (current == null) {
+          parent.left = n
+          break
         }
       } else {
-        current = current.right;
-        if(current == null) {
-          parent.right = n;
-          break;
+        current = current.right
+        if (current == null) {
+          parent.right = n
+          break
         }
       }
     }
   }
 }
 
-//traversal Depth
-function inOrder(node) {
+// traversal Depth
+function inOrder (node) {
   if(node != null) {
     inOrder(node.left)
     console.log(node.show())
@@ -50,26 +49,24 @@ function inOrder(node) {
   }
 }
 
-//traversal Width
-function preOrder(node) {
+// traversal Width
+function preOrder (node) {
   if(node !=null) {
-    console.log(node.show());
+    console.log(node.show())
     preOrder(node.left)
     preOrder(node.right)
   }
 }
 
-function postOrder(node) {
-  if(node !=null) {
+function postOrder (node) {
+  if (node != null) {
     postOrder(node.left)
     postOrder(node.right)
     console.log(node.show())
   }
 }
 
-
-
-var nums = new BST();
+var nums = new BST()
 nums.insert(23)
 nums.insert(45)
 nums.insert(16)
@@ -77,10 +74,10 @@ nums.insert(37)
 nums.insert(3)
 nums.insert(99)
 nums.insert(22)
-console.log('Traversal In Order :');
-inOrder(nums.root);
-console.log('Traversal Pre Order:');
+console.log('Traversal In Order :')
+inOrder(nums.root)
+console.log('Traversal Pre Order:')
 preOrder(nums.root)
 
-console.log('Traversal Post Order');
+console.log('Traversal Post Order')
 postOrder(nums.root)
