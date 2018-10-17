b7543fcdd156873e46954afc63546e5f4e6aeae3
Carmen Liu <carmen.liujia@gmail.com>
2018-10-17 15:28:34 +1300

    algorithm update

--- 11.graphs/grape_adjacency_matrix.js
+++ 11.graphs/grape_adjacency_matrix.js
@@ -1,35 +1,36 @@
-function Vertex(label) {
-  this.label = label;
+function Vertex (label) {
+  this.label = label
 }
-function Graph(v) {
-  this.vertices = v;
-  this.edge = 0;
-  this.adj = [];
-  for(var i=0;i<this.vertices; ++i) {
+function Graph (v) {
+  this.vertices = v
+  this.edge = 0
+  this.adj = []
+  for (var i = 0; i < this.vertices; ++i) {
     this.adj[i] = []
   }
   this.addEdge = addEdge
   this.showGraph = showGraph
 }
 
-function addEdge(v, w) {
+function addEdge (v, w) {
   this.adj[v].push(w)
   this.adj[w].push(v)
 }
-function showGraph() {
+function showGraph () {
   for (var i = 0; i < this.vertices; ++i) {
-    process.stdout.write(i + " -> ");
+    process.stdout.write(i + ' -> ')
     for (var j = 0; j < this.vertices; ++j) {
-    if (this.adj[i][j] != undefined) 
-      process.stdout.write(this.adj[i][j] + ' ');
+      if (this.adj[i][j] !== undefined) {
+        process.stdout.write(this.adj[i][j] + ' ')
+      }
     }
-    console.log("");
+    console.log('')
   }
 }
 
-g = new Graph(5);
-g.addEdge(0,1);
-g.addEdge(0,2);
-g.addEdge(1,3);
-g.addEdge(2,4);
-g.showGraph();
+let g = new Graph(5)
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 3)
+g.addEdge(2, 4)
+g.showGraph()
